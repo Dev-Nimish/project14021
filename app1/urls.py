79dc87842b7e1444998da40b9015b3311b4bdfcc
Nimish Jain <nimishjain.005@gmail.com>
2021-04-16 23:26:25 +0530

    New Function

--- app1/urls.py
+++ app1/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.if_statement, name = "if_demo"),
     path('if_else/',views.if_else, name = "if_else_demo"),
     path('for/',views.for_loop, name = "for"),
+    path('for1/',views.for1, name = "for1"),
 ]
--- app1/views.py
+++ app1/views.py
@@ -12,4 +12,8 @@ def if_else(request):
 
 def for_loop(request):
     login = True
-    return render(request,"for_loop.html",context = {'items':['Apple','Ball','Cat'],'profile':{'name':'Nimish','age':24,'sal':101202000}})
+    return render(request,"for_loop.html",context = {'items':['Apple','Ball','Cat'],'profile':{'name':'Nimish','age':24,'sal':101202123}})
+
+def for_loop1(request):
+    login = True
+    return render(request,"for_loop.html",context = {'items':['Aim','Book','Dog'],'profile':{'name':'Teja','age':24,'sal':101202123}})
